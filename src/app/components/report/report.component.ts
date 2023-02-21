0cbdc003e74ce7b0a3333e82c8b75cfabc8271fc
Julian Pulecio <juliandavidpuleciogomez@gmail.com>
2023-02-21 13:52:56 -0500

    feature: se agrega el formulario y listado de reportes para infracciones usando el email

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -13,6 +13,8 @@ export class ReportComponent implements OnInit {
 
   constructor(private person_service:PersonService, private router: Router) { }
 
+  vehicles:Array<any> = [] 
+
   form_data:FormGroup = new FormGroup({ 
     email: new FormControl(),
   });
@@ -23,10 +25,18 @@ export class ReportComponent implements OnInit {
   onClickSubmit(value:any){
     this.person_service.get_report(this.form_data.value).subscribe({
       next: (data) => { 
-        console.log(data)
+        this.vehicles = data
       },
       error: (error:any) => {
-        console.log(error)
+        const validation_errors = error.error
+        Object.keys(validation_errors).forEach(prop=>{
+          const formControl = this.form_data.get(prop)
+          if(formControl){
+            formControl.setErrors(validation_errors[prop])
+          } else {
+            this.form_data.setErrors(validation_errors[prop])
+          }
+        })
       }
     })
   }
